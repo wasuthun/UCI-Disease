bdf777554cbb359a6299a56eb03cf0ffb9c6c046
wasuthun wanaphongthipakorn <pond_Zen@hotmail.com>
2019-04-25 19:32:57 +0700

    add comment in code

--- UCI-Disease.py
+++ UCI-Disease.py
@@ -5,7 +5,6 @@ Created on Mon Apr 22 11:41:54 2019
 
 @author: johnpaul
 """
-
 import numpy as np
 import pandas as pd
 from keras.models import Sequential
@@ -21,24 +20,28 @@ from IPython import get_ipython
 get_ipython().run_line_magic('matplotlib', 'inline')
 import warnings
 warnings.filterwarnings("ignore")
-
+'''
+check missing value function
+'''
 def null_table(data):
     print("Training Data Frame")
     print(pd.isnull(data).sum()) 
 df=pd.read_csv("/Users/johnpaul/Downloads/heart.csv")
-'''
-print(df.info())
 print(null_table(df))
-plt.figure(figsize=(10,8))
-sns.heatmap(df.corr(method='pearson'),annot=True,cmap='YlGnBu',fmt='.2f',linewidths=2)
+print(df.info())
+print(df.head())
 '''
-print(df.target.value_counts())
+Show a number people who have heart disease and don't have.
+'''
 sns.countplot(x="target", data=df, palette="bwr")
 plt.show()
 countNoDisease = len(df[df.target == 0])
 countHaveDisease = len(df[df.target == 1])
 print("Percentage Haven't Heart Disease: {:.2f}%".format((countNoDisease / (len(df.target))*100)))
 print("Percentage Have Heart Disease: {:.2f}%".format((countHaveDisease / (len(df.target))*100)))
+'''
+Show a number of sex of people in dataset.
+'''
 sns.countplot(x='sex', data=df, palette="mako_r")
 plt.xlabel("Sex (0 = female, 1= male)")
 plt.show()
@@ -46,13 +49,22 @@ countFemale = len(df[df.sex == 0])
 countMale = len(df[df.sex == 1])
 print("Percentage of Female : {:.2f}%".format((countFemale / (len(df.sex))*100)))
 print("Percentage of Male : {:.2f}%".format((countMale / (len(df.sex))*100)))
+'''
+groupby heart disease
+'''
 print(df.groupby('target').mean())
+'''
+Show bar graph Heart Disease Frequency for Ages
+'''
 pd.crosstab(df.age,df.target).plot(kind="bar",figsize=(20,6))
 plt.title('Heart Disease Frequency for Ages')
 plt.xlabel('Age')
 plt.ylabel('Frequency')
 plt.savefig('heartDiseaseAndAges.png')
 plt.show()
+'''
+Show bar graph Heart Disease Frequency for Sex
+'''
 pd.crosstab(df.sex,df.target).plot(kind="bar",figsize=(15,6),color=['#1CA53B','#AA1111' ])
 plt.title('Heart Disease Frequency for Sex')
 plt.xlabel('Sex (0 = Female, 1 = Male)')
@@ -60,18 +72,27 @@ plt.xticks(rotation=0)
 plt.legend(["Haven't Disease", "Have Disease"])
 plt.ylabel('Frequency')
 plt.show()
+'''
+Show scatter plot of Heart Disease occure wit maximun heart rate and age
+'''
 plt.scatter(x=df.age[df.target==1], y=df.thalach[(df.target==1)], c="red")
 plt.scatter(x=df.age[df.target==0], y=df.thalach[(df.target==0)])
 plt.legend(["Disease", "Not Disease"])
 plt.xlabel("Age")
 plt.ylabel("Maximum Heart Rate")
 plt.show()
+'''
+Show bar graph Heart Disease Frequency for slope of peak
+'''
 pd.crosstab(df.slope,df.target).plot(kind="bar",figsize=(15,6),color=['#DAF7A6','#FF5733' ])
 plt.title('Heart Disease Frequency for Slope')
 plt.xlabel('The Slope of The Peak Exercise ST Segment ')
 plt.xticks(rotation = 0)
 plt.ylabel('Frequency')
 plt.show()
+'''
+Show bar graph Heart Disease Frequency for Fasting blood sugar
+'''
 pd.crosstab(df.fbs,df.target).plot(kind="bar",figsize=(15,6),color=['#FFC300','#581845' ])
 plt.title('Heart Disease Frequency According To FBS')
 plt.xlabel('FBS - (Fasting Blood Sugar > 120 mg/dl) (1 = true; 0 = false)')
@@ -79,12 +100,18 @@ plt.xticks(rotation = 0)
 plt.legend(["Haven't Disease", "Have Disease"])
 plt.ylabel('Frequency of Disease or Not')
 plt.show()
+'''
+Show bar graph Heart Disease Frequency for type of chest pain
+'''
 pd.crosstab(df.cp,df.target).plot(kind="bar",figsize=(15,6),color=['#11A5AA','#AA1190' ])
 plt.title('Heart Disease Frequency According To Chest Pain Type')
 plt.xlabel('Chest Pain Type')
 plt.xticks(rotation = 0)
 plt.ylabel('Frequency of Disease or Not')
 plt.show()
+'''
+Preprocessing
+'''
 a = pd.get_dummies(df['cp'], prefix = "cp")
 b = pd.get_dummies(df['thal'], prefix = "thal")
 c = pd.get_dummies(df['slope'], prefix = "slope")
@@ -94,9 +121,14 @@ print(df.head())
 df = df.drop(columns = ['cp', 'thal', 'slope'])
 y = df.target.values
 x_data = df.drop(['target'], axis = 1)
-#Normalize
+'''
+Normalize
+'''
 x = (x_data - np.min(x_data)) / (np.max(x_data) - np.min(x_data)).values
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.2,random_state=0)
+'''
+Build Neuro network model and prediction
+'''
 model = Sequential()
 model.add(Dense(11,activation='relu',input_dim=21))
 model.add(Dense(4,activation='relu'))
@@ -104,17 +136,20 @@ model.add(Dropout(rate=0.5))
 model.add(Dense(3,activation='relu'))
 model.add(Dense(3,activation='relu'))
 model.add(Dense(1,activation='sigmoid'))
-
 model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
-model.fit(x_train,y_train,epochs=300)
+model.fit(x_train,y_train,epochs=100)
 sd=model.predict(x_test)
-print(sd)
-print(y_test)
 suk=accuracy_score(y_test,sd.round())
 print("Test Accuracy of NeuroNet: {:.2f}%".format(suk*100))
+'''
+Use SVC from sklean and check accuracy score by use testing data
+'''
 svm = SVC(random_state = 1)
 svm.fit(x_train, y_train)
 print("Test Accuracy of SVM Algorithm: {:.2f}%".format(svm.score(x_test,y_test)*100))
+'''
+Use Random Forest from sklean and check accuracy score by use testing data
+'''
 rf = RandomForestClassifier(n_estimators = 1000, random_state = 1)
 rf.fit(x_train, y_train)
 print("Random Forest Algorithm Accuracy Score : {:.2f}%".format(rf.score(x_test,y_test)*100))
